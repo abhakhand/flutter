bfff9d62c7d401c1bc570d09d13c480a40884e2d
abhakhand <reachbhakhand@gmail.com>
2023-02-18 22:59:36 +0530

    feat: cleanup

--- weather/test/helpers/pump_app.dart
+++ weather/test/helpers/pump_app.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:weather/l10n/l10n.dart';
+import 'package:weather/app/l10n/l10n.dart';
 
 extension PumpApp on WidgetTester {
   Future<void> pumpApp(Widget widget) {
